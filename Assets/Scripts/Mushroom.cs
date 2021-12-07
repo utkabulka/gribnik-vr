ba60fbec8515019d57a502bb27b7eb535dcbd473
Roman Zubin <utkabulka@gmail.com>
2021-12-07 17:18:09 +0300

    korzina

--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -5,8 +5,17 @@ using UnityEngine;
 public class Mushroom : MonoBehaviour
 {
     public MushroomData mushroomData;
+    private bool isPickedUp = false;
+
+    private void Start() {
+        GetComponent<Rigidbody>().mass = this.transform.localScale.x;
+    }
 
     public int GetPoints() {
         return (int)(this.transform.localScale.x * mushroomData.BasePoints);
+    }
+
+    public void SetPickedUp(bool value) {
+        isPickedUp = value;
     }
 }
